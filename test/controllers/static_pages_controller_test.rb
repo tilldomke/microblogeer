753ddd4aa935eb78a4568a51dde87bb755e1aa23
tilldomke <tilldomke@gmail.com>
2020-07-09 15:32:15 +0200

    Changed root

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -30,4 +30,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title","Contact | #{@base_title}"
   end
 
+  test "should get root" do 
+    get root_url
+    assert_response :success
+  end
+
 end
