fb47d08a7802ae06bf73a66db9ae9d420c5efed6
tilldomke <tilldomke@gmail.com>
2020-07-26 22:41:58 +0200

    added DB indices

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+before_save {self.email = email.downcase}
+
     validates :name, 
         presence: true, 
         length: {maximum: 50}
@@ -8,5 +11,5 @@ class User < ApplicationRecord
         presence: true, 
         length: {maximum: 255}, 
         format: { with: VALID_EMAIL_REGEX },
-        uniqueness: {case_sensitive: false}
+        uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_20_131207) do
+ActiveRecord::Schema.define(version: 2020_07_26_165951) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
