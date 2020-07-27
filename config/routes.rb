1c0f765fb4a35ec995d59e38ec97b35e14908248
tilldomke <tilldomke@gmail.com>
2020-07-27 21:37:12 +0200

    User Name and mail now shows in GET route

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   get "/signup", to: "users#new"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+
+  resources :users
 end
